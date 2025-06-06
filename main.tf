defa4efcc9d78e2fd20e17d4bd215055aea0d980
kiran90-gh <kiranmyself90@gmail.com>
2025-06-06 10:37:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web" {
+/*resource "aws_instance" "web" {
   ami           = "ami-06031e2c49c278c8f"
   instance_type = "t2.micro"
    count=1
@@ -6,4 +6,4 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
     test = "dev"
   }
-}
+}*/
